1f0b3c7f5eb0c868d24286765e538ffd9bd35e8f
Vishal Vijay <0vishalvijay0@gmail.com>
2014-02-03 21:36:45 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Missile::Application.config.secret_key_base = 'cd087a2a1e4fbc4c40f2ea65441c1fe246d76e9abf9a5f2ff6026f3f8baa9e88907518e1abf0241580bd41a9ef553a94135180867085d7e38409e05a9c8c5a0e'
